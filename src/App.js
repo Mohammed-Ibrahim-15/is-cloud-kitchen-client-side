e20f76db9a690cb2d7df173abc84aa370fb9907b
Mohammed-Ibrahim-15 <ibrahim72342@gmail.com>
2022-11-07 22:58:17 +0600

    firebase added

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <h1 className='text-6xl'>Hello</h1>
+      <button className="btn btn-outline btn-warning">Warning</button>
     </div>
   );
 }
