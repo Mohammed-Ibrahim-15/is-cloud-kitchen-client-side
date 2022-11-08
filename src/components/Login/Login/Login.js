194181f626fe3a68b3b68a41160d344c67f6efb5
Mohammed-Ibrahim-15 <ibrahim72342@gmail.com>
2022-11-08 19:04:09 +0600

    email password login complete

--- src/components/Login/Login/Login.js
+++ src/components/Login/Login/Login.js
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../../context/AuthProvider/AuthProvider';
 
 const Login = () => {
+    const { login } = useContext(AuthContext)
     const handleLogin = (event) => {
         event.preventDefault()
         const form = event.target
         const email = form.email.value
         const password = form.password.value
-        console.log(email, password)
+        // console.log(email, password)
+
+        login(email, password)
+            .then(result => {
+                const user = result.user
+                console.log(user)
+            })
+            .catch(err => console.error(err))
     }
     return (
         <div>
