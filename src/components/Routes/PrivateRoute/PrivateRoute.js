0afe2cfc10d008859f2f22e4d2afd58eae1ac564
Mohammed-Ibrahim-15 <ibrahim72342@gmail.com>
2022-11-08 20:33:27 +0600

    spinner added

--- src/components/Routes/PrivateRoute/PrivateRoute.js
+++ src/components/Routes/PrivateRoute/PrivateRoute.js
@@ -7,7 +7,13 @@ const PrivateRoute = ({ children }) => {
     const location = useLocation();
 
     if (loading) {
-        return <h1>Loading.............</h1>
+        return <div>
+            <div class="flex justify-center items-center">
+                <div class="spinner-border animate-spin inline-block w-8 h-8 border-4 rounded-full" role="status">
+                    <span class="visually-hidden">Loading...</span>
+                </div>
+            </div>
+        </div>
     }
 
     if (user) {
